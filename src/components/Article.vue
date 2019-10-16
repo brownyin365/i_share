65afdfd7322ccff0734bdf9444d2e99afe331820
Robert Yin <31589648+brownyin365@users.noreply.github.com>
2019-10-16 06:20:23 -0700

    article.vue
    Add image to home page

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -2,6 +2,7 @@
     <div class="card my-3">
         <div class="card-header font-weight-bolder">{{article.title}}</div>
             <div class="card-body">
+                <img height="420px" :src="article.imageUrl" alt="" class="card-img-top">
                 <div class="my-3" v-html="article.content.substring(0, 200)"></div>               
                 
                 <router-link :to="`/article/${article.slug}`" class="btn btn-primary">Read More</router-link>
